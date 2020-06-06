4d32cb39735715e477e439d0b327f7b5bd55da1e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-06 22:37:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,17 +16,6 @@ monopoly = {
 end
 
 def monopoly_with_second_tier
-	monopoly = {
-		:railroads => {pieces:4, names:{reading_railroad: {},
-				pennsylvania_railroad: {},
-				b_and_o_railroad: {},
-				shortline: {}
-}, rent_in_dollars:{
-		one_piece_owned:25, two_pieces_owned:50, three_pieces_owned:100, four_pieces_owned:200}}
-	}
-end
-
-
 monopoly = {
 		:railroads => {
 			pieces: 4,
@@ -42,6 +31,7 @@ monopoly = {
 
 
 end
+
 
 
 
